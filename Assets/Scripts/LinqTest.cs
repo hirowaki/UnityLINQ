db4eaca6408dba8418c7ad98595b6ce163a64b4d
hirowaki <hirowaki0314@gmail.com>
2016-10-11 21:53:27 -0700

    UniRX

--- Assets/Scripts/LinqTest.cs
+++ Assets/Scripts/LinqTest.cs
@@ -2,23 +2,33 @@
 using System.Collections;
 using System.Collections.Generic;  
 using System.Linq;
+using UniRx;
 
 public class LinqTest {
-    private static string OutputLog<T>(string title, IEnumerable<T> e) {
+    private static float _testInterval = 1.0f;
+
+    // Generic is simlar to template in C++. But generated dinamically.
+    // No static memory mapping at preprocess. 
+    private static string FormatResult<T>(string title, IEnumerable<T> e) {
         // _.map, Enumerable<int> => Enumerable<string> => ToArray.
         var str = title + " [" + string.Join(",", e.Select(i => i.ToString()).ToArray()) + "]";
         Debug.Log(str);
         return str;
     }
 
-    private static string OutputLog<T>(string title, T v) {
+    private static string FormatResult<T>(string title, T v) {
         var str = title + "[" + v.ToString() + "]";
         Debug.Log(str);
         return str;
     }
 
-    public static void RunningSamples () {
+    // RunningSamples of LINQ. 
+    // The functions themselves are similar to lodash/underscore in JS.
+    // JS.Object would be C#.Enumerable here.
+    public static IEnumerator RunningSamples (IObserver<string> observer) {
         var source = Enumerable.Range(0, 11).ToArray();
+
+        string result = null;
 
         var testNum = 1;
         while (testNum > 0) {
@@ -26,72 +36,76 @@ public class LinqTest {
             case 1:
                 // _.filter in JS.
                 var filtered = source.Where(i => ((i & 1) == 0));
-                OutputLog("Where", filtered);  // => 0, 2, 4, 8, 10
+                result = FormatResult("Where", filtered);  // => 0, 2, 4, 8, 10
                 break;
             case 2:
                 // _.map in JS.
                 var mapped = source.Select(i => (i << 1));
-                OutputLog("Select", mapped);  // => 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+                result = FormatResult("Select", mapped);  // => 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
                 break;
             case 3:
                 // _.take / _.slice in JS. taking the three elements from the head.
                 var takenHead = source.Take(3);
-                OutputLog("Take", takenHead);  // => 0, 1, 2
+                result = FormatResult("Take", takenHead);  // => 0, 1, 2
                 break;
             case 4:
                 // _.filter(a => a < 5)
                 var takenWhile = source.TakeWhile(i => i < 5);
-                OutputLog("TakeWhile", takenWhile);  // => 0, 1, 2, 3, 4
+                result = FormatResult("TakeWhile", takenWhile);  // => 0, 1, 2, 3, 4
                 break;
             case 5:
                 // takenWhile would break the loop right away when the condition fails. 
                 var takenWhile2 = source.TakeWhile(i => ((i & 1) == 0));
-                OutputLog("TakeWhile", takenWhile2);  // => 0.
+                result = FormatResult("TakeWhile", takenWhile2);  // => 0.
                 break;
             case 6:
                 // _.head.
                 var first = source.First();
-                OutputLog("First", first);  // => 0
+                result = FormatResult("First", first);  // => 0
                 break;
             case 7:
                 // First can have a condition.
                 var first2 = source.First(i => i > 2);
-                OutputLog("First", first2);  // => 3
+                result = FormatResult("First", first2);  // => 3
                 break;
             case 8:
                 // FirstOrDefault.
                 var firstD = source.FirstOrDefault(i => i > 100);   // no such elements in the souce.
-                OutputLog("FirstOrDefault", firstD);  // => 0. then default<T>(=> 0) would come.
+                result = FormatResult("FirstOrDefault", firstD);  // => 0. then default<T>(=> 0) would come.
                 break;
             case 9:
                 // Count.
                 var count = source.Count();
-                OutputLog("Count", count);  // 10
+                result = FormatResult("Count", count);  // 10
                 break;
             case 10:
                 // Count. (_.filter().length).
                 var count2 = source.Count(i => ((i & 1) != 0));
-                OutputLog("Count", count2); // 5
+                result = FormatResult("Count", count2); // 5
                 break;
             case 11:
                 // _.every.
                 var all = source.All(i => (i < 1000));  // all elements would satisfy this condition.
-                OutputLog("All", count);  // True
+                result = FormatResult("All", all);  // True
                 break;
             case 12:
                 // _.isEmpty.
                 var any = source.Any();
-                OutputLog("Any", any);  // True
+                result = FormatResult("Any", any);  // True
                 break;
             case 13:
                 // _.some.
                 var some = source.Any(i => (i > 100));    // no elements will satisfy the condition (i > 100).
-                OutputLog("Any", some);  // False.
+                result = FormatResult("Any", some);  // False.
 
                 testNum = -1;
                 break;
             }
+
+            observer.OnNext(result); 
+            yield return new WaitForSeconds(_testInterval);
         }
 
+        observer.OnCompleted();
     }
 }
--- Assets/Scripts/MainScene.cs
+++ Assets/Scripts/MainScene.cs
@@ -1,37 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UniRx;
 
 public class MainScene : MonoBehaviour {
+    private string _result = "START";
 
-	// Use this for initialization
-	void Start () {
-        LinqTest.RunningSamples();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
+    // static.
     private static GUIStyle _guiStyle = new GUIStyle();
     private static Rect _guiRect = new Rect();
     private static Color _colorWhite = new Color(1, 1, 1, 1);
 
-    static GUIStyle GetGUIStyle() {
-        return _guiStyle ?? (_guiStyle = new GUIStyle());
-    }
+    void Start () {
+        Observable.FromCoroutine <string> (observer => LinqTest.RunningSamples (observer))
+            .Subscribe (
+                // kinda Promise(resolve(args)) in JS. (But Rx and Promise are not the same).
+                result => {
+                    _result = result;
+                },
+                () => {
+                    _result="COMPLETED";
+                }
+            );
+	}
 
     void OnGUI() {
-        GetGUIStyle().fontSize = 32;
-        GetGUIStyle().normal.textColor = _colorWhite;
-        GetGUIStyle().alignment = TextAnchor.MiddleCenter;
+        _guiStyle.fontSize = 32;
+        _guiStyle.normal.textColor = _colorWhite;
+        _guiStyle.alignment = TextAnchor.MiddleCenter;
 
-        Rect rect = _guiRect;
-        rect.width = 128;
-        rect.height = 32;
-        rect.x = Screen.width / 2 - rect.width / 2;;
-        rect.y = Screen.height / 2 - rect.height / 2;;
+        _guiRect.width = 128;
+        _guiRect.height = 32;
+        _guiRect.x = Screen.width / 2 - _guiRect.width / 2;;
+        _guiRect.y = Screen.height / 2 - _guiRect.height / 2;;
 
-        GUI.Label(rect, "Sample", GetGUIStyle());
+        GUI.Label(_guiRect, _result, _guiStyle);
     }
 }
